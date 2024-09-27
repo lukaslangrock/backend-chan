adc2ffe0a96169a47565e6b0aa6ed38173a94bd9
seanm <sean.maier@outlook.de>
2024-09-27 19:56:01 -0400

    Added UserLoginRequest.cs

---
+++ src/UserLoginRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace backend;
+
+public class UserLoginRequest
+{
+    private string _username;
+    private string _password;
+
+    public string Username
+    {
+        get => _username;
+        set => _username = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => _password = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public UserLoginRequest(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
