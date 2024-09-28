d182966badf5d49dd923840e2c94906b2ee3f62b
Hendrik <99285389+Flopticode@users.noreply.github.com>
2024-09-28 10:11:54 +0200

    Added send/receive code

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,24 @@
 using System.Net;
+using System.Net.WebSockets;
 using System.Text;
+using backend;
+
+int receiveBufferSize = 0xFFFF;
+int sendBufferSize = 0xFFFF;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Connichiwa, backuendo-chan heru. Pleasu connectu on webu sokketsu to interufacu.");
 
+WebSocket? ws = null;
+
 app.UseWebSockets();
 app.Map("/ws", async context =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
-        using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        var bytes = Encoding.UTF8.GetBytes("Hello from Server");
-        await ws.SendAsync(new ArraySegment<byte>(bytes, 0, bytes.Length), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+        ws = await context.WebSockets.AcceptWebSocketAsync();
     }
     else
     {
@@ -21,4 +26,50 @@ app.Map("/ws", async context =>
     }
 });
 
-await app.RunAsync();
+app.RunAsync();
+
+var serverBuffer = WebSocket.CreateServerBuffer(receiveBufferSize);
+
+async Task<string?> Receive()
+{
+    var result = await ws.ReceiveAsync(serverBuffer, CancellationToken.None);
+    
+    if (result.MessageType == WebSocketMessageType.Text)
+    {
+        return System.Text.Encoding.UTF8.GetString(serverBuffer.Array, serverBuffer.Offset, result.Count);
+    }
+    return null;
+}
+
+Task<string?>? receiveTask = null;
+Task? sendTask = null;
+
+while (true)
+{
+    if (ws == null)
+        continue;
+
+    if(receiveTask == null)
+        receiveTask = Receive();
+
+    if (receiveTask.GetAwaiter().IsCompleted)
+    {
+        var result = receiveTask.GetAwaiter().GetResult();
+
+        if (result != null)
+        {
+            string? response = ProtocolHandler.OnReceive(result);
+            if (response != null)
+            {
+                var bytes = Encoding.UTF8.GetBytes(response);
+
+                if (sendTask != null && !sendTask.IsCompleted)
+                    await sendTask;
+
+                sendTask = ws.SendAsync(new ArraySegment<byte>(bytes, 0, bytes.Length),
+                    System.Net.WebSockets.WebSocketMessageType.Text,
+                    true, CancellationToken.None);
+            }
+        }
+    }
+}
