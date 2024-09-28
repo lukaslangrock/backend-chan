2b98fe85397dcd3d36a3e040232990e521d0d475
seanm <sean.maier@outlook.de>
2024-09-27 20:55:54 -0400

    Added ServerRoomLayout.cs

---
+++ src/ProtocolObjects/ServerRoomLayout.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.ProtocolObjects;
+
+public class ServerRoomLayout(RoomDescription[] roomDescription)
+{
+    private readonly RoomDescription[] _roomDescription = roomDescription;
+}
