d5c16fbafefc3244e6a0614ddd462fba5441dd9c
Hendrik <99285389+Flopticode@users.noreply.github.com>
2024-09-28 09:28:40 +0200

    Added ProtocolHandler
    don't look for the implementation. it's not there yet. 404. nullptr. if you use it, you will segfault

---
+++ src/ProtocolHandler.cs
@@ -0,0 +1,14 @@
+namespace backend;
+
+public static class ProtocolHandler
+{
+    public static void OnReceive(string json)
+    {
+        
+    }
+
+    public static void Send(string json)
+    {
+        
+    }
+}
