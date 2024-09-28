df5284c06a200194104245eab1fcdf26c7403caa
Hendrik <99285389+Flopticode@users.noreply.github.com>
2024-09-28 14:16:06 +0200

    Added Shakespearean Translator

---
+++ src/LLMController.cs
@@ -0,0 +1,50 @@
+using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+
+namespace backend;
+
+public class LlmController
+{
+    public static readonly string SystemPrompt =
+        "You are the Shakespeare translator. You translate chat messages to Shakespearean language. You use old, fancy, shakespearean language and high class words.";
+
+    private static OpenAIService openAiService;
+    
+    public static async void Init()
+    {
+        openAiService = new OpenAIService(new OpenAiOptions()
+        {
+            ApiKey = File.ReadAllText("resources/OpenAI_APIKey.apikey")
+        });
+    }
+
+    public static async Task<string?> TranslateToShakespeare(string text)
+    {
+        var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+        {
+            Messages = new List<ChatMessage>
+            {
+                ChatMessage.FromSystem(SystemPrompt),
+                ChatMessage.FromUser("Hey, what's up?"),
+                ChatMessage.FromAssistant("Hark! What tidings dost thou bring upon this fine day?"),
+                ChatMessage.FromUser(text)
+            },
+            Model = Models.Gpt_4o_mini ,
+            MaxTokens = 100,
+        });
+        
+        if (completionResult.Successful)
+        {
+            var generatedMessage = completionResult.Choices.First().Message.Content;
+            Console.WriteLine("Message: " + generatedMessage);
+            return generatedMessage;
+        }
+        
+        Console.WriteLine("Shakespeare's tongue is broken.");
+        Console.WriteLine(completionResult.Error.Message);
+
+        return null;
+    }
+}
