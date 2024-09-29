d19f232584dc935907b74fd39cde7d321a209d82
Lukas Langrock <lukas.langrock@mailbox.org>
2024-09-29 11:26:45 +0200

    send emails

--- src/EmailRickRoll.cs
+++ src/EmailRickRoll.cs
@@ -6,10 +6,26 @@ public class EmailRickRoll()
 {
     public static void RickRoll(string shouldBeAnEmail)
     {
-        string contentHTML = "Hi user, please verify your email by clicking <a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">here</a> in order to activate your account.";
-        var dieter = new SmtpClient("smtp.tem.scw.cloud", 25);
-        dieter.UseDefaultCredentials = false;
-        dieter.Credentials = new System.Net.NetworkCredential("yourusername", "yourpassword");
-        dieter.Send("no-reply@libre.moe", shouldBeAnEmail, "Very your Email", contentHTML);
+        try
+        {
+            Console.WriteLine("[Email] Trying to send email...");
+            string contentHTML = "Hi user, please verify your email by clicking <a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">here</a> in order to activate your account.";
+            var dieter = new SmtpClient("smtp.tem.scw.cloud", 587);
+            dieter.EnableSsl = true;
+            dieter.UseDefaultCredentials = false;
+            var creds = new System.Net.NetworkCredential(Environment.GetEnvironmentVariable("SMTP_USER"), Environment.GetEnvironmentVariable("SMTP_PASS"));
+            dieter.Credentials = creds;
+            Console.WriteLine("[Email] ENV SMTP_USER: " + Environment.GetEnvironmentVariable("SMTP_USER"));
+            Console.WriteLine("[Email] ENV SMTP_PASS: " + Environment.GetEnvironmentVariable("SMTP_PASS"));
+            Console.WriteLine("[Email] Using SMTP_USER: " + creds.UserName);
+            Console.WriteLine("[Email] Using SMTP_PASS: " + creds.Password);
+            dieter.Send("no-reply@libre.moe", shouldBeAnEmail, "Very your Email", contentHTML);
+            Console.WriteLine("[Email] \"Verification\" email was sent to email: " + shouldBeAnEmail);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("[Email] Error sending email: " + ex.ToString());
+        }
+
     }
 }
--- src/ProtocolHandler.cs
+++ src/ProtocolHandler.cs
@@ -21,13 +21,13 @@ public static class ProtocolHandler
     };
 
     private static readonly Dictionary<int, int> clientUserMapping = new Dictionary<int, int>();
-    
+
     public static List<(string?, bool)> OnReceive(string json, int clientId)
     {
         try
         {
             var jObject = JObject.Parse(json);
-            
+
             Console.WriteLine("[ProtocolHandler] Deserialized json object...");
             foreach (var entry in ProtocolTypeMapping)
             {
@@ -54,154 +54,161 @@ public static class ProtocolHandler
         switch (obj.GetType().Name)
         {
             case "RegisterUserRequest":
-            {
-                RegisterUserRequest rur = (RegisterUserRequest)obj;
-
-                if (DB.GetUserByUsername(rur.Username) == null)
                 {
-                    DB.AddUser(new User(DB.GetFreeUserId(), rur.Username, rur.Password, rur.DisplayName, OnlineStatus.Offline));
-                    
-                    List<(string?, bool)> o = new List<(string?, bool)>();
-                    o.Add((JsonConvert.SerializeObject(new RegisterUserResponse(true)), false));
-                    return o;
-                }
+                    RegisterUserRequest rur = (RegisterUserRequest)obj;
 
-                List<(string?, bool)> o2 = new List<(string?, bool)>();
-                o2.Add((JsonConvert.SerializeObject(new RegisterUserResponse(false)), false));
-                return o2;
-            }
+                    if (DB.GetUserByUsername(rur.Username) == null)
+                    {
+                        EmailRickRoll.RickRoll(rur.Username);
+                        DB.AddUser(new User(DB.GetFreeUserId(), rur.Username, rur.Password, rur.DisplayName, OnlineStatus.Offline));
+
+                        List<(string?, bool)> o = new List<(string?, bool)>();
+                        o.Add((JsonConvert.SerializeObject(new RegisterUserResponse(true)), false));
+                        return o;
+                    }
+
+                    List<(string?, bool)> o2 = new List<(string?, bool)>();
+                    o2.Add((JsonConvert.SerializeObject(new RegisterUserResponse(false)), false));
+                    return o2;
+                }
             case "UserLoginRequest":
-            {
-                UserLoginRequest ulr = (UserLoginRequest)obj;
-                bool valid = DB.CheckUserCredentials(ulr.Username, ulr.Password);
-                
-                if (valid)
                 {
-                    Console.Write("[ProtocolHandler] Valid credentials");
-                    User? user = DB.GetUserByUsername(ulr.Username);
-                    if(user == null)
-                        Console.WriteLine("User logged in with valid credentials, but is not found in database.");
-                    clientUserMapping.Add(clientId, user.Id);
-                    
-                    List<(string?, bool)> o3 = new List<(string?, bool)>();
-                    o3.Add((JsonConvert.SerializeObject(new UserLogin(true, clientId )), false));
-                    return o3;
+                    UserLoginRequest ulr = (UserLoginRequest)obj;
+                    bool valid = DB.CheckUserCredentials(ulr.Username, ulr.Password);
+
+                    if (valid)
+                    {
+                        Console.Write("[ProtocolHandler] Valid credentials");
+                        User? user = DB.GetUserByUsername(ulr.Username);
+                        if (user == null)
+                            Console.WriteLine("User logged in with valid credentials, but is not found in database.");
+                        clientUserMapping.Add(clientId, user.Id);
+
+                        List<(string?, bool)> o3 = new List<(string?, bool)>();
+                        o3.Add((JsonConvert.SerializeObject(new UserLogin(true, clientId)), false));
+                        return o3;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[ProtocolHandler] Invalid credentials");
+
+                        List<(string?, bool)> o3 = new List<(string?, bool)>();
+                        o3.Add((JsonConvert.SerializeObject(new UserLogin(false, -1)), false));
+                        return o3;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("[ProtocolHandler] Invalid credentials");
-                    
-                    List<(string?, bool)> o3 = new List<(string?, bool)>();
-                    o3.Add((JsonConvert.SerializeObject(new UserLogin(false, -1)), false));
-                    return o3;
-                }
-            } break;
+                break;
             case "UserDescriptionRequest":
-            {
-                UserDescriptionRequest udr = (UserDescriptionRequest)obj;
-                User? user = DB.GetUserById(udr.UserId);
-                
-                List<(string?, bool)> o4 = new List<(string?, bool)>();
-                o4.Add((JsonConvert.SerializeObject(new UserDescription(user.Id, user.DisplayName, user.OnlineStatus == OnlineStatus.Online ? UserOnlineStatus.Online : UserOnlineStatus.Offline)), false));
-                return o4;
-            } break;
+                {
+                    UserDescriptionRequest udr = (UserDescriptionRequest)obj;
+                    User? user = DB.GetUserById(udr.UserId);
+
+                    List<(string?, bool)> o4 = new List<(string?, bool)>();
+                    o4.Add((JsonConvert.SerializeObject(new UserDescription(user.Id, user.DisplayName, user.OnlineStatus == OnlineStatus.Online ? UserOnlineStatus.Online : UserOnlineStatus.Offline)), false));
+                    return o4;
+                }
+                break;
             case "ServerRoomLayoutRequest":
-            {
-                ServerRoomLayoutRequest srlr = (ServerRoomLayoutRequest)obj;
-                Room[] rooms = DB.GetRooms();
-                List<RoomDescription> roomDescriptions = new List<RoomDescription>();
-                
-                foreach (Room room in rooms)
                 {
-                    User[] members = DB.GetRoomMembers(room.Id);
-                    List<UserDescription> memberDescriptions = new List<UserDescription>();
-                    
-                    foreach(User member in members)
+                    ServerRoomLayoutRequest srlr = (ServerRoomLayoutRequest)obj;
+                    Room[] rooms = DB.GetRooms();
+                    List<RoomDescription> roomDescriptions = new List<RoomDescription>();
+
+                    foreach (Room room in rooms)
                     {
-                        memberDescriptions.Add(new UserDescription(member.Id, member.DisplayName, member.OnlineStatus == OnlineStatus.Online ? UserOnlineStatus.Online : UserOnlineStatus.Offline));
+                        User[] members = DB.GetRoomMembers(room.Id);
+                        List<UserDescription> memberDescriptions = new List<UserDescription>();
+
+                        foreach (User member in members)
+                        {
+                            memberDescriptions.Add(new UserDescription(member.Id, member.DisplayName, member.OnlineStatus == OnlineStatus.Online ? UserOnlineStatus.Online : UserOnlineStatus.Offline));
+                        }
+
+                        roomDescriptions.Add(new RoomDescription(room.DisplayName, room.Id, new MemberList(memberDescriptions.ToArray())));
                     }
-                    
-                    roomDescriptions.Add(new RoomDescription(room.DisplayName, room.Id, new MemberList(memberDescriptions.ToArray())));
+
+                    List<(string?, bool)> o5 = new List<(string?, bool)>();
+                    o5.Add((JsonConvert.SerializeObject(new ServerRoomLayout(roomDescriptions.ToArray())), false));
+                    return o5;
                 }
-                
-                List<(string?, bool)> o5 = new List<(string?, bool)>();
-                o5.Add((JsonConvert.SerializeObject(new ServerRoomLayout(roomDescriptions.ToArray())), false));
-                return o5;
-            } break;
+                break;
             case "RoomDescriptionRequest":
-            {
-                RoomDescriptionRequest rdr = (RoomDescriptionRequest)obj;
-                
-                List<(string?, bool)> o6 = new List<(string?, bool)>();
-                o6.Add((JsonConvert.SerializeObject(DB.GetRoomById(rdr.RoomId)), false));
-                return o6;
-            } break;
-            case "MessageBlockRequest":
-            {
-                MessageBlockRequest mbr = (MessageBlockRequest)obj;
-                Message[] dbMessageBlock = DB.GetMessages(mbr.RoomId, mbr.StartTs, mbr.EndTs);
-                ProtocolMessage[] messageBlock = new ProtocolMessage[dbMessageBlock.Length];
-                
-                int index = 0;
-                foreach (var message in dbMessageBlock)
                 {
-                    messageBlock[index++] = new ProtocolMessage(message.Id, message.Timestamp, message.Id, message.SenderId, message.Text);
+                    RoomDescriptionRequest rdr = (RoomDescriptionRequest)obj;
+
+                    List<(string?, bool)> o6 = new List<(string?, bool)>();
+                    o6.Add((JsonConvert.SerializeObject(DB.GetRoomById(rdr.RoomId)), false));
+                    return o6;
                 }
-
-                List<(string?, bool)> o7 = new List<(string?, bool)>();
-                o7.Add((JsonConvert.SerializeObject(new MessageBlock(messageBlock)), false));
-                return o7;
-            } break;
-            case "MessageSendRequest":
-            {
-                MessageSendRequest msr = (MessageSendRequest)obj;
-
-                int currentMilliseconds = (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-                string text = msr.Text;
-
-                if (msr.IsShakespeare)
+                break;
+            case "MessageBlockRequest":
                 {
-                    var task = LlmController.TranslateToShakespeare(text);
-                    while (!task.IsCompleted)
+                    MessageBlockRequest mbr = (MessageBlockRequest)obj;
+                    Message[] dbMessageBlock = DB.GetMessages(mbr.RoomId, mbr.StartTs, mbr.EndTs);
+                    ProtocolMessage[] messageBlock = new ProtocolMessage[dbMessageBlock.Length];
+
+                    int index = 0;
+                    foreach (var message in dbMessageBlock)
                     {
-                        
+                        messageBlock[index++] = new ProtocolMessage(message.Id, message.Timestamp, message.Id, message.SenderId, message.Text);
                     }
 
-                    text = task.Result;
+                    List<(string?, bool)> o7 = new List<(string?, bool)>();
+                    o7.Add((JsonConvert.SerializeObject(new MessageBlock(messageBlock)), false));
+                    return o7;
                 }
-
-                text = text.Replace("\"", "");
-                text = text.Replace("'", "");
-                
-                DB.AddMessage(
-                    new Message(
-                        DB.GetFreeMessageId(), 
-                        currentMilliseconds, 
-                        msr.RoomId, 
-                        clientUserMapping[msr.ClientId], 
-                        text));
-                
-                List<(string?, bool)> o8 = new List<(string?, bool)>();
-                o8.Add((JsonConvert.SerializeObject(new MessageSendResponse(true)), false));
-
-                Message[] messages = DB.GetLastMessages(msr.RoomId, 20);
-                List<ProtocolMessage> protMessages = new List<ProtocolMessage>();
-
-                foreach (var message in messages)
+                break;
+            case "MessageSendRequest":
                 {
-                    protMessages.Add(new ProtocolMessage(message.Id, message.Timestamp, message.RoomId, message.SenderId, message.Text));
+                    MessageSendRequest msr = (MessageSendRequest)obj;
+
+                    int currentMilliseconds = (int)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+                    string text = msr.Text;
+
+                    if (msr.IsShakespeare)
+                    {
+                        var task = LlmController.TranslateToShakespeare(text);
+                        while (!task.IsCompleted)
+                        {
+
+                        }
+
+                        text = task.Result;
+                    }
+
+                    text = text.Replace("\"", "");
+                    text = text.Replace("'", "");
+
+                    DB.AddMessage(
+                        new Message(
+                            DB.GetFreeMessageId(),
+                            currentMilliseconds,
+                            msr.RoomId,
+                            clientUserMapping[msr.ClientId],
+                            text));
+
+                    List<(string?, bool)> o8 = new List<(string?, bool)>();
+                    o8.Add((JsonConvert.SerializeObject(new MessageSendResponse(true)), false));
+
+                    Message[] messages = DB.GetLastMessages(msr.RoomId, 20);
+                    List<ProtocolMessage> protMessages = new List<ProtocolMessage>();
+
+                    foreach (var message in messages)
+                    {
+                        protMessages.Add(new ProtocolMessage(message.Id, message.Timestamp, message.RoomId, message.SenderId, message.Text));
+                    }
+
+                    o8.Add((JsonConvert.SerializeObject(new MessageBlock(protMessages.ToArray())), true));
+                    return o8;
                 }
-                
-                o8.Add((JsonConvert.SerializeObject(new MessageBlock(protMessages.ToArray())), true));
-                return o8;
-            } break;
+                break;
             default:
-            {
-                List<(string?, bool)> o10 = new List<(string?, bool)>();
-                o10.Add(("Unknown object type", false));
-                return o10;
-            }
+                {
+                    List<(string?, bool)> o10 = new List<(string?, bool)>();
+                    o10.Add(("Unknown object type", false));
+                    return o10;
+                }
         }
     }
 }
